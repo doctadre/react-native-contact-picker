e8fddaf3ad3faa75b055e73b1833b137f9b10945
Andre Harris <andre.harris@build.com>
2016-05-05 10:33:56 -0700

    Request contact permissions so we can use linked contacts

--- ios/ContactPicker.m
+++ ios/ContactPicker.m
@@ -15,6 +15,8 @@
 @property (nonatomic, strong) ABPeoplePickerNavigationController *pickerController;
 @property (nonatomic, strong) RCTPromiseResolveBlock resolve;
 @property (nonatomic, strong) RCTPromiseRejectBlock reject;
+@property (nonatomic, assign) ABAddressBookRef addressBookRef;
+
 @end
 
 @implementation ContactPicker
@@ -29,6 +31,7 @@ RCT_EXPORT_MODULE();
     while (self.root.presentedViewController != nil) {
         self.root = self.root.presentedViewController;
     }
+    self.addressBookRef = ABAddressBookCreateWithOptions(NULL, NULL);
     self.pickerController = [[ABPeoplePickerNavigationController alloc] init];
     return self;
 }
@@ -40,7 +43,18 @@ RCT_REMAP_METHOD(pickContact,resolver:(RCTPromiseResolveBlock)resolve
     self.resolve = resolve;
     self.reject = reject;
     self.pickerController.peoplePickerDelegate = self;
-    [self.root presentViewController:self.pickerController animated:YES completion:nil];
+    
+    if (ABAddressBookGetAuthorizationStatus() != kABAuthorizationStatusAuthorized) {
+        ABAddressBookRequestAccessWithCompletion(self.addressBookRef, ^(bool granted, CFErrorRef error) {
+            if (granted) {
+                [self.root presentViewController:self.pickerController animated:YES completion:nil];
+            } else {
+                self.reject(@"access_denied", @"We need access to your contacts to use this feature, please go into your settings and enable it.",(__bridge NSError *)error);
+            }
+        });
+    } else {
+      [self.root presentViewController:self.pickerController animated:YES completion:nil];
+    }
 }
 - (void)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker
                          didSelectPerson:(ABRecordRef)person {
